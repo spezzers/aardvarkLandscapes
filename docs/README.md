799198afda77f22b7dd1626b51d77d1f48cb5fe0
spezzers <54096233+spezzers@users.noreply.github.com>
2019-08-16 13:34:50 +0100

    tidy up unnecessary files

--- docs/README.md
+++
@@ -1,2 +0,0 @@
-# aardvark-landscapes.uk
- Local Business Website
--- docs/testScript.js
+++
@@ -1,6 +0,0 @@
-var i = 0;
-setInterval(function() {
-    console.log('iteration ' + i)
-    i++;
-}, 1000)
-// This really doesn't need to be here at all
