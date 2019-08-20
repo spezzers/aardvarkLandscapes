654b1ff2fcfe9f90f429b96be96e0cfb5a64a144
spezzers <54096233+spezzers@users.noreply.github.com>
2019-08-20 18:24:19 +0100

    sitemap, robots, file cleanup

--- testScript.js
+++
@@ -1,4 +0,0 @@
-function email(extension, domain, user) {
-    document.write('<a hr'+ 'ef="' + 'ma' + 'il' + 'to:' + user + '@' + domain + '.' + extension + '">' + user + '@' + domain + '.' + extension + '</a>')
-}
-email("com", "gmail", "contact.aardvark");
