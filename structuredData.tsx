3cb7332f9cc396a7671407a9de8c7c2abaa829da
spezzers <54096233+spezzers@users.noreply.github.com>
2020-11-30 08:34:09 +0000

    typo correction

--- structuredData.tsx
+++ structuredData.tsx
@@ -5,7 +5,7 @@ script type = "application/ld+json" > {
     "@type": "HomeAndConstructionBusiness",
     "name": "Aardvark Landscapes",
     "sameAs": "https://www.facebook.com/",
-    "description": "Based in Leicestershire, Aardvark are a duo of landscapers experienced in both commercial and private landscape gardening and tree surgery. Weather you are after some routine garden maintenance, or more substantial landscaping, Aardvark offer a friendly, quality service."
+    "description": "Based in Leicestershire, Aardvark are a duo of landscapers experienced in both commercial and private landscape gardening and tree surgery. Whether you are after some routine garden maintenance, or more substantial landscaping, Aardvark offer a friendly, quality service."
     "address": {
         "@type": "PostalAddress",
         "streetAddress": "29 Waterloo Cresent",
