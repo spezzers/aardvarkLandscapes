dd9a5b331c769f96e185c5b8169099935dc7dc82
spezzers <54096233+spezzers@users.noreply.github.com>
2020-12-13 09:46:49 +0000

    restructure files

--- aardvark-gatsby/gatsby-config.js
+++ aardvark-gatsby/gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
       resolve: "gatsby-plugin-mdx",
       options: {
         defaultLayouts: {
-          default: require.resolve("./src/components/default-page-layout.mdx"),
+          default: require.resolve("./src/layouts/default-page-layout.mdx"),
         },
       },
     },
